caa82aa4cd6e77047bb012366b98efa5999bebef
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-10-03 17:22:22 -0500

    feat: implement editor class

--- src/video/logic/editor.py
+++ src/video/logic/editor.py
@@ -1,7 +1,43 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+import ffmpeg
 from ffmpeg.nodes import InputNode
+
 # import ffmpeg
+
+
+@dataclass
+class ClipSettings:
+    frame_rate: int | None = None
+    output_path: Path | None = None
+    mirror: bool = False
+    clip_duration: float | None = None
+    clip_fps: float | None = None
+    mode: list[str] = ["video"]  # video | gif
+
+    # I dont think we need more for now
+
+
+class Clip:
+    """Clip cerator"""
+
+    def __init__(self, video: InputNode, settings: ClipSettings):
+        self.video = video
+        self.output_path: Path
+        self.settings = settings
+
+    def create_clips(self) -> list[Path]:
+        pass
 
 
 class Editor:
     def __init__(self, video: InputNode):
-        pass
+        self.video = video
+
+    @property
+    def clip(self, settings: ClipSettings) -> Clip:
+        return Clip(
+            self.video,
+            settings,
+        )
