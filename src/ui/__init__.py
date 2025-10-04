5aaf00f764103e7e5bc71eddc93b38830cedfcec
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-10-04 05:31:23 -0500

    feat: implement typer UI

---
+++ src/ui/__init__.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import typer
+
+from video.logic.editor import Clip, ClipMode, ClipSettings
+
+from .. import __version__
+
+# video-editor
+# schema: video-editor edit seconds input output format
+
+app = typer.Typer(
+    name="video editor",
+    help="A video editing script using ffmpeg",
+    add_completion=False,
+)
+
+
+@app.command()
+def edit(seconds: int, mode_input: str, input: str) -> None:
+    mode = []
+
+    match mode_input:
+        case "gif":
+            mode += [ClipMode.GIF]
+        case "mp4":
+            mode += [ClipMode.VIDEO]
+        case "both":
+            mode += [ClipMode.VIDEO, ClipMode.GIF]
+
+    path = Path(input)
+    settings = ClipSettings(clip_length=seconds, mode=mode)
+
+    editor = Clip(
+        settings=settings,
+        input_path=path,
+        output_path=None,
+    )
+    editor.create_clips()
+
+    typer.secho("Success")
+
+
+@app.command()
+def version() -> None:
+    """Show version information."""
+    typer.secho(f"version: {__version__}", fg=typer.colors.CYAN)
+
+
+@app.command()
+def help() -> None:
+    """Show help"""
+    typer.secho("Configuration:", fg=typer.colors.YELLOW, bold=True)
+
+
+if __name__ == "__main__":
+    app()
