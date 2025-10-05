592f7b5de9f12efc1c53af52c31600a842eac181
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-10-04 21:59:09 -0500

    refactor: move main to a function

--- src/__main__.py
+++ src/__main__.py
@@ -6,8 +6,14 @@ from typer.colors import RED
 from .ui import app
 from .utils import ffmpeg_install_check
 
-if __name__ == "__main__":
+
+def main():
+    """Main entry point for the CLI application."""
     if not ffmpeg_install_check():
         secho("ffmpeg is not installed", fg=RED)
         exit()
     app()
+
+
+if __name__ == "__main__":
+    main()
